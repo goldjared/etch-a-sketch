d26aa5a46aec284de3e5dff54cb6ffd7ccfa459a
Jared Gold <114547220+goldjared@users.noreply.github.com>
2022-12-12 22:50:30 -0500

    Create 16 div cells in js, create js file

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ gridCells();
 
 function gridCells () {
     const container = document.querySelector('.container');
+    container.style.display = 'grid';
     for (i = 0; i < 16; i++) {
         const cell = document.createElement('div');
         cell.className = 'grid-cell';
+        cell.textContent = i;
         container.appendChild(cell);
 
     }
