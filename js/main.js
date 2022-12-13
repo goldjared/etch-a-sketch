3e41c8fa53155637d6c7d4bd123f661cf7e2f216
Jared Gold <114547220+goldjared@users.noreply.github.com>
2022-12-13 15:10:25 -0500

    Correct grid as 16x16, add correct css in js, set up mouseover function

--- js/main.js
+++ js/main.js
@@ -2,13 +2,26 @@ gridCells();
 
 function gridCells () {
     const container = document.querySelector('.container');
-    container.style.display = 'grid';
-    for (i = 0; i < 16; i++) {
+    const gridStyles = `
+        display: grid;
+        grid-template-columns: repeat(16, 1fr);
+        margin: 0;
+        border: 0;`;
+    container.style.cssText = gridStyles;
+    
+    for (i = 1; i <= 16 * 16; i++) {
         const cell = document.createElement('div');
         cell.className = 'grid-cell';
         cell.textContent = i;
         container.appendChild(cell);
 
     }
+    container.addEventListener('mouseover', drawing);
 }
+
+function drawing() {
+    console.log('it works');
+}
+
+
 
